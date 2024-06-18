0079015f903a9ccf85a0836ee50e30c4abb429ed
Leon Cruz <leon.cruz.teixeira@gmail.com>
2024-06-17 23:46:48 -0300

    Refactor 😅

---
+++ .config/fish/config.fish
@@ -0,0 +1,26 @@
+#! /usr/bin/fish
+
+set -Ux TERM alacritty
+
+if status is-interactive
+  and not set -q TMUX
+    exec tmux
+end
+
+function fish_user_key_bindings
+  bind -M insert \ce accept-autosuggestion
+end
+
+set fish_greeting
+
+function be
+  bundle exec $argv
+end
+
+function reload
+  . ~/.config/fish/config.fish
+end
+
+~/.local/bin/mise activate fish | source
+
+starship init fish | source
