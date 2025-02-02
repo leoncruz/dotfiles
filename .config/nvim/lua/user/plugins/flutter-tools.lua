3d6e35c14629dc16e992c28a29613609a8cea44f
Leon Cruz <leon.cruz.teixeira@gmail.com>
2025-02-01 22:35:23 -0300

    Autoformat for dart files

--- .config/nvim/lua/user/plugins/flutter-tools.lua
+++ .config/nvim/lua/user/plugins/flutter-tools.lua
@@ -5,6 +5,11 @@ return {
 		dev_log = {
 			open_cmd = "-1tabnew"
 		},
-		flutter_lookup_cmd = "mise where flutter"
+		flutter_lookup_cmd = "mise where flutter",
+		lsp = {
+			on_attach = function()
+				vim.api.nvim_create_autocmd({ "BufWritePre" }, { pattern = "*.dart", command = ":lua vim.lsp.buf.format()" })
+			end
+		}
 	}
 }
