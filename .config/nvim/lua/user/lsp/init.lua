4cc8651da9074289242d53578542d0441bfa911d
Leon Cruz <leon.cruz.teixeira@gmail.com>
2025-06-05 22:33:07 -0300

    Support to open file on ruby-lsp

--- .config/nvim/lua/user/lsp/init.lua
+++ .config/nvim/lua/user/lsp/init.lua
@@ -7,7 +7,42 @@ cmp.setup()
 
 lsp_config.ruby_lsp.setup {
 	capabilities = cmp_capabilities,
-	filetypes = { "ruby", "erb", "eruby" }
+	filetypes = { "ruby", "erb", "eruby" },
+	on_attach = function(client, bufnr)
+    vim.lsp.commands['rubyLsp.openFile'] = function(command, ctx)
+      local uri = command.arguments[1][1]
+
+      local filename = vim.uri_to_fname(uri)
+
+			local options = {
+				{ label = "Open file", command = "edit" },
+				{ label = "Open file in new tab", command = "tabedit" },
+				{ label = "Split vertical", command = "vsplit" },
+				{ label = "Split horizontal", command = "split" },
+			}
+
+			vim.ui.select(options, {
+				prompt = "Como deseja abrir o arquivo?",
+				format_item = function(item) return item.label end,
+			}, function(choice)
+				if not choice then return end
+				vim.cmd(choice.command .. " " .. vim.fn.fnameescape(filename))
+			end)
+    end
+
+		vim.api.nvim_create_autocmd({ "BufEnter", "InsertLeave" }, {
+			buffer = bufnr,
+			callback = function()
+				if client.supports_method("textDocument/codeLens") then
+					vim.lsp.codelens.refresh()
+				end
+			end,
+		})
+
+		if client.supports_method("textDocument/codeLens") then
+			vim.lsp.codelens.refresh()
+		end
+	end
 }
 
 lsp_config.jedi_language_server.setup {
