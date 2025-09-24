a636c6edbf87817475fae055a1c332410401872f
Leon Cruz <leon.cruz.teixeira@gmail.com>
2025-09-24 19:15:56 -0300

    Fix identation

--- .config/nvim/lua/user/plugins/emmet-vim.lua
+++ .config/nvim/lua/user/plugins/emmet-vim.lua
@@ -1,15 +1,15 @@
 return {
 	"kmoschcau/emmet-vim",
-		ft = {
-			"html",
-			"javascriptreact",
-			"javascript",
-			"typescriptreact",
-			"eelixir",
-			"eruby",
-			"elixir"
-		},
-		config = function()
-			vim.cmd([[ imap <expr> <tab> emmet#expandAbbrIntelligent("\<tab>") ]])
-		end,
+	ft = {
+		"html",
+		"javascriptreact",
+		"javascript",
+		"typescriptreact",
+		"eelixir",
+		"eruby",
+		"elixir"
+	},
+	config = function()
+		vim.cmd([[ imap <expr> <tab> emmet#expandAbbrIntelligent("\<tab>") ]])
+	end,
 }
